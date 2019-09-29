617ec460326ab6c396ee95d919053c953f3d164c
Paul Rosero <paul_rosero@yahoo.com>
2019-09-29 17:14:03 -0400

    create table for each massage_spas, massage_therapists, clients

--- db/migrate/20190929205753_create_massage_spas.rb
+++ db/migrate/20190929205753_create_massage_spas.rb
@@ -1,6 +1,10 @@
 class CreateMassageSpas < ActiveRecord::Migration
   def change
     create_table :massage_spas do |t|
+      t.string "email"
+      t.string "username"
+      t.string "password_digest"
+      t.string "name"
 
       t.timestamps null: false
     end
--- db/migrate/20190929205820_create_clients.rb
+++ db/migrate/20190929205820_create_clients.rb
@@ -1,6 +1,10 @@
 class CreateClients < ActiveRecord::Migration
   def change
     create_table :clients do |t|
+      t.string "name"
+      t.string "address"
+      t.string "medical_history"
+      t.integer "massage_spa_id"
 
       t.timestamps null: false
     end
