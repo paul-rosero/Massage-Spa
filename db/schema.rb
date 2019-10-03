db11d446eee9c3fd1731588ffa2ff95108ceb43c
Paul Rosero <paul_rosero@yahoo.com>
2019-10-03 11:34:30 -0400

    delete the model,views,db of massage_therapist.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190929205844) do
+ActiveRecord::Schema.define(version: 20190929205820) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "name"
@@ -28,15 +28,6 @@ ActiveRecord::Schema.define(version: 20190929205844) do
     t.string   "name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-  end
-
-  create_table "massage_therapists", force: :cascade do |t|
-    t.string   "name"
-    t.string   "modalities"
-    t.string   "bio"
-    t.integer  "massage_spa_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
   end
 
 end
