ea0ecd2db57cf24d28e0d0d82935c7bb8d4359c9
Paul Rosero <paul_rosero@yahoo.com>
2019-10-04 00:08:25 -0400

    delete the signup get and post request.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,28 +17,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  get "/signup" do
-    if logged_in?
-      
-      redirect "/massage_spas/new.html"
-    else
-      erb :"/sessions/new.html"
-     
-    end
-  end
-
-  post "/signup" do
-    @massage_spa = MassageSpa.new(name: params[:name], email: params[:email], password: params[:password])
-    if @massage_spa.save
-      session[:massage_spa_id] = @massage_spa.id
-
-      redirect '/massage_spas/index.html'
-    else
-       redirect "/signup"
-    end
-  end
-
-  # GET: /sessions/new
   get "/logout" do
     session.clear
     erb :"/sessions/logout.html"
