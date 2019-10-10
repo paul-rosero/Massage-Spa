c13a1a4778cdb75ffee16680dd6cf11f616ecfd0
Paul Rosero <paul_rosero@yahoo.com>
2019-10-10 19:02:21 -0400

    clear commented code in my routes and indented properly.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,21 +3,23 @@ class ClientsController < ApplicationController
   get "/clients" do
     if logged_in?
       @clients = Client.all
+
       erb :"/clients/index.html"
     else
+
       redirect '/'
     end
   end
 
-  
   get "/clients/new" do
     if logged_in?
+
       erb :"/clients/new.html"
     else
+
       redirect '/'
     end
   end
-
   
   post "/clients" do
     if logged_in?
@@ -30,6 +32,7 @@ class ClientsController < ApplicationController
         redirect '/clients/new'
       end
     else
+
       redirect '/'
     end
   end
@@ -37,44 +40,45 @@ class ClientsController < ApplicationController
   get "/clients/:id" do
     if logged_in?
       @client = Client.find_by(id: params[:id])
+
       erb :"/clients/show.html"
     else
+
       redirect '/'
     end
   end
 
-  
   get "/clients/:id/edit" do
     @client = Client.find_by(id: params[:id])
-    if @client.massage_spa == current_user #logged_in? 
+    if @client.massage_spa == current_user 
         
-        erb :"/clients/edit.html"
-      #else
-       # redirect "/massage_spas/#{current_user.id}"
-     # end
+      erb :"/clients/edit.html"
     else
-      redirect '/'
+
+      redirect "/massage_spas/#{current_user.id}"
     end
   end
-
   
   patch "/clients/:id" do
     @client = Client.find_by(id: params[:id])
     if @client.massage_spa == current_user
         @client.update(name: params[:name], address: params[:address], medical_history: params[:medical_history])
+        
         redirect "/clients/#{@client.id}"
       else
+        
       redirect '/'
     end
   end
-
 
   delete "/clients/:id" do
     @client = Client.find_by(id: params[:id])
     if @client.massage_spa == current_user
       @client.destroy
+
       redirect "/clients"
     else
+
       redirect "/massage_spas/#{current_user.id}"
     end
   end
